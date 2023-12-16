709baa5ab14bc691938940697124c53dfe47a626
fotuka <antda08@yandex.ru>
2023-12-16 15:52:50 +0300

    fail

--- src/script.js
+++ src/script.js
@@ -4,9 +4,7 @@ import { data } from "../src/mock/data.js";
 import { OptionsComponent } from "./component/OptionsComponent.js";
 
 const bodyElement = document.querySelector(".app-body");
-
 const optionsComponent = new OptionsComponent(["123","234","345"]);
-
 const comboBoxComponent = new ComboBoxComponent(optionsComponent.getTemplate());
 
 render(comboBoxComponent, bodyElement);
